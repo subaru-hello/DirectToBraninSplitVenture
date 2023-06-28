5c2b0666c19071b9910723c4e649fcff15b78198
subaru-hello <octpsubaru@icloud.com>
2023-06-28 22:13:07 +0900

    コマンドラインからの入力を待つ

--- showa/sample.py
+++ showa/sample.py
@@ -5,11 +5,12 @@ load_dotenv()  # take environment variables from .env.
 
 # os.environ["OPENAI_API_KEY"] = os.getenv('DATABASE_URL')
 
-import logging
+# import logging
 import sys
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+args = sys.argv
+first_arg = args[1] if len(args) > 1 else None
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 # 読み込み
 documents = SimpleDirectoryReader('spilitsVentures').load_data()
@@ -20,9 +21,9 @@ index = VectorStoreIndex.from_documents(documents)
 
 # 検索
 query_engine = index.as_query_engine()
-
+user_input = input("何を聞きたいですか？: ")
 # 検索結果
-response = query_engine.query("ベンチャーの定義は？")
+response = query_engine.query(user_input)
 
 
 print(response)
