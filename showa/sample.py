9694b29888a79e1bfb8c048fed84028e3d46f057
subaru-hello <octpsubaru@icloud.com>
2023-06-29 09:00:28 +0900

    add gradio

--- showa/sample.py
+++ showa/sample.py
@@ -1,17 +1,14 @@
 import os
+import sys
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
-
-# os.environ["OPENAI_API_KEY"] = os.getenv('DATABASE_URL')
+load_dotenv()
 
 # import logging
-import sys
 # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
-args = sys.argv
-first_arg = args[1] if len(args) > 1 else None
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
 # 読み込み
 documents = SimpleDirectoryReader('spilitsVentures').load_data()
 # documents = SimpleDirectoryReader('/data').load_data()
@@ -22,8 +19,9 @@ index = VectorStoreIndex.from_documents(documents)
 # 検索
 query_engine = index.as_query_engine()
 user_input = input("何を聞きたいですか？: ")
+with_japanese = "日本語で回答してください。"
 # 検索結果
-response = query_engine.query(user_input)
+response = query_engine.query(user_input + with_japanese)
 
 
 print(response)
